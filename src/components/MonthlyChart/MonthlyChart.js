d5986cddef71ae0cb64f36075816efdbcb9de25d
HANEUL ROH <haneulroh@HANEULs-MacBook-Air.local>
2023-09-07 10:39:46 -0400

    finished all the features + dashboard updated

---
+++ src/components/MonthlyChart/MonthlyChart.js
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Box, Typography } from '@mui/material';
+import axios from 'axios';
+import { Chart as ChartJS, CategoryScale, Title, Tooltip } from 'chart.js';
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
+const MonthlyChart = () => {
+  const [climbingSessions, setClimbingSessions] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${SERVER_URL}/climbingsession`) 
+      .then((res) => {
+        setClimbingSessions(res.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  // grouping the date by month to show by month
+  const groupByMonth = (data) => {
+    //need to sort because of database sort date by desc
+    const sortedData = [...data].sort((a, b) => {
+      const dateA = new Date(a.session_date);
+      const dateB = new Date(b.session_date);
+      return dateA - dateB;
+    });
+
+    const groupedData = {}; //empty object
+    sortedData.forEach((session) => {
+        //Date format
+      const sessionDate = new Date(session.session_date);
+      const year = sessionDate.getFullYear();
+      const month = sessionDate.getMonth();
+      const monthKey = `${year}-${month}`;
+      // year and month needed only
+      if (!groupedData[monthKey]) {
+        groupedData[monthKey] = {
+          monthLabel: getMonthLabel(sessionDate),
+          days: {},
+        };
+      }
+      const dayOfMonth = sessionDate.getDate();
+      groupedData[monthKey].days[dayOfMonth] = true;
+    });
+    return groupedData;
+  };
+
+  const getMonthLabel = (date) => {
+    const monthNames = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+    return monthNames[date.getMonth()];
+  };
+
+  const groupedData = groupByMonth(climbingSessions);
+  const labels = Object.values(groupedData).map((monthData) => monthData.monthLabel);
+  const dayCounts = Object.values(groupedData).map((monthData) =>
+    Object.keys(monthData.days).length
+  );
+
+  ChartJS.register(Title, Tooltip, CategoryScale);
+
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Days Climbed',
+        data: dayCounts,
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <Box>
+      <Bar
+        data={chartData}
+        options={{
+          scales: {
+            x: {
+              title: {
+                display: false,
+                text: 'Month',
+              },
+            },
+            y: {
+              title: {
+                display: true,
+                text: 'Days Climbed',
+              },
+              beginAtZero: true,
+              max: 31, // Month has 31 days
+            },
+          },
+        }}
+      />
+    </Box>
+  );
+};
+
+export default MonthlyChart;
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -3,81 +3,80 @@ import { tokens } from "../../theme";
 import "./Dashboard.scss";
 import mountNemo from "../../assets/images/mount-nemo.jpg";
 import { Link } from "react-router-dom";
+import MonthlyChart from "../../components/MonthlyChart/MonthlyChart";
 
 const Dashboard = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   return (
-    <Box color={colors.grey[100]}>
+    <>
       <Box
         display="flex"
         flexDirection="column"
         justifyContent="center"
         alignItems="center"
+        color={colors.grey[100]}
+        margin="1.5rem"
       >
         <Typography mt="1rem" variant="h2" fontWeight="600">
           Welcome to{" "}
-          <span style={{ color: `${colors.greenAccent[600]}` }}>
+          <span style={{ color: `${colors.greenAccent[600]}`, marginBottom: "2rem" }}>
             ClimbStation 🧗‍♀️
           </span>
         </Typography>
       </Box>
-      <Box display="flex" margin="2rem">
+
+      <Box margin="auto" width="50vw" color={colors.grey[100]}>
+        <MonthlyChart />
+      </Box>
+      <Box>
         <Box
-          margin="0 2rem"
-          padding="1.5rem"
-          backgroundColor={colors.grey[200]}
-          borderRadius="0.5rem"
-          maxHeight="70vh"
+          color={colors.grey[100]}
+          margin="1.5rem auto"
+          width="50vw"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
         >
-          <img
-            src={mountNemo}
-            alt="Mount Nemo pic"
-            style={{ width: "40vw", height: "60vh" }}
-          />
-          <Typography
-            mr="1rem"
-            textAlign="right"
-            color={colors.blueAccent[600]}
-          >
-            Mount Nemo photo by Sky
+          <Typography variant="h3" marginBottom="1.5rem">
+            ClimbStation is an all-in-one solution designed for climbers,
+            providing them with the tools they need to enhance their climbing
+            experience.
+          </Typography>
+          <Typography variant="h3">
+            From tracking workouts and creating packing lists to offering
+            real-time weather updates for outdoor climbing, this app is a
+            must-have companion for both indoor and outdoor climbers.
           </Typography>
         </Box>
-        <Box m="0 0 0 2rem" maxHeight="70vh" overflow="auto">
-          <Typography variant="h4" mb="1.5rem">
-            ClimbStation is your ultimate climbing companion, designed for both
-            indoor and outdoor enthusiasts. Whether you're scaling artificial
-            walls or conquering natural peaks, we've got everything you need to
-            enhance your climbing journey.
-          </Typography>
-          <Typography variant="h4" mb="1.5rem">
-            At ClimbStation, we're dedicated to elevating your climbing
-            experience. Our platform offers a range of features to empower
-            climbers of all levels. From tracking your climbing sessions to
-            providing essential packing lists and up-to-date weather forecasts
-            for outdoor, we're here to support your climbing adventures.
-          </Typography>
-          <Typography variant="h4" mb="1.5rem">
-            Join our vibrant community of climbers and adventurers. Whether
-            you're a seasoned pro or just starting your climbing journey,
-            ClimbStation is your partner in reaching new heights. Unleash your
-            potential, explore new challenges, and achieve your climbing goals
-            with ClimbStation. Your next climb begins here.
-          </Typography>
-
-          <Link to="climbingtracker"
+        <Box
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Link
+            to="climbingtracker"
             style={{
               textDecoration: "none",
               color: `${colors.blueAccent[600]}`,
+              textAlign: "center",
             }}
           >
-            <Button variant="h1"  textAlign="right" sx={{ bgcolor: "white", fontWeight:"700"}}>
+            <Button
+              variant="h1"
+              sx={{ bgcolor: `${colors.primary[100]}`, fontWeight: "700" }}
+            >
               Get Started
             </Button>
           </Link>
         </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
